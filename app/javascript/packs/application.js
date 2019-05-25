5497c6740f67ec4a469b29f6c5a46cc8a7efa949
alex <alex_is_ok@yahoo.com>
2019-05-25 12:12:18 +0300

    bootstrap vue added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,16 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+/* require("@rails/ujs").start() */
+/* require("turbolinks").start() */
+require("@rails/activestorage").start();
+require("channels");
 
+import Vue from 'vue/dist/vue.esm.js';
+import BootstrapVue from 'bootstrap-vue';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
